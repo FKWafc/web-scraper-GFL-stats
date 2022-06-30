f4710bc6c4ed165a67e80706a311caa8e7b5a38c
FKWafc <finn.kearns@gmail.com>
2022-06-30 22:52:14 +0100

    Working on sorting the data from the HTML

--- main.py
+++ main.py
@@ -11,12 +11,39 @@ stats_html_tags = soup.find_all('th', style="text-align:left;")
 stats_html_data = soup.find_all('td', style="text-align:left;")
 
 #simple way of printing the text in the tables. Still very unstructured
+headings = []
+# i think a 2d array might work here to sort the text into lists determined by stat catagory
 for tags in stats_html_tags:
-    for data in stats_html_data:
+    if tags.text in headings:
+        pass
+    else:
+        headings.append(tags.text)
+        data1 = tags.text
+        data2 = []
+        for data in stats_html_data:
+
+            # this line is currently broken
+            x = soup.find_all('td', style="text-align:left;", text=True).text
+            if x in data2:
+                pass
+            else:
+                data2.append(x)
+        print(data2)
         #print(tags.text)
         #print(data.text)
-        pass
-# this is an example of a dataframe that I would like to use to structure the data
+"""
+df = pd.DataFrame(
+    data in stats_html_data,
+    columns=headings,
+)
+print(df)
+"""
+
+
+
+# this is an example of a dataframe that I would like to use to structure the data (not working)
+
+"""
 df = pd.DataFrame(
     stats_html_data,
     columns=[
@@ -33,3 +60,4 @@ df = pd.DataFrame(
     ],
 )
 print(df)
+"""
